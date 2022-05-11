acbacd5550e0e39b571e966bb76580ceb0f1a13d
oybek13 <oybek.karimjanov13@gmail.com>
2022-05-11 18:35:48 +0500

    Jwt Done!

--- src/main/java/davr/team/security/JwtAuthenticationFilter.java
+++ src/main/java/davr/team/security/JwtAuthenticationFilter.java
@@ -1,7 +1,6 @@
 package davr.team.security;
 
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
